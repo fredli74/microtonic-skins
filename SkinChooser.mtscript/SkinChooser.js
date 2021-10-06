7aeffc15b1fe37dfce00f49f8b5724f55a492246
Fredrik Lidström <fredli74@gmail.com>
2021-10-07 00:06:05 +0200

    Removing SkinChooser script from skin repository

--- SkinChooser.mtscript/SkinChooser.js
+++
@@ -1,9 +0,0 @@
-if (BUILD.NUMBER < 874) {
-    if (display("Skins are not supported by this Microtonic version. Do you want to go to https://soniccharge.com and download a newer version?", "question", "yes no") === "yes") {
-        performCushyAction("launch", "https://soniccharge.com");
-    }
-} else if (!(function() { try { dir(DIRS.SKINS) } catch (x) { return false; }; return true; })()) {
-    display('"Microtonic Skins" folder is missing.\n\nThe path to this folder is: ' + DIRS.SKINS);
-} else {
-    toggleCushy('SkinChooser.mtscript/SkinChooser_main');
-}
